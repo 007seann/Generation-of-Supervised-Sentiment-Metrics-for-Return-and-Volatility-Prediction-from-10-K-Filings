4b71534dc2d0758cfabc3a16abde89a412b2dfe0
007seann <sean.sanggyu@gmail.com>
2024-12-21 18:51:32 +0000

    completed the code to collect scripts

--- Code_4_transcripts/estimate_scriptCount.py
+++ Code_4_transcripts/estimate_scriptCount.py
@@ -7,7 +7,6 @@ import pandas as pd
 
 # Global variables
 id_folder = "transcript_ids"
-save_folder = "transcript_count"
 year_since = 2006
 year_until = 2024
 
@@ -33,50 +32,23 @@ except UnicodeDecodeError:
     cik_ticker = dict(zip(cik, ticker))
     
 
-async def fetch_ids_for_ticekr(ticker: str, semaphore: asyncio.Semaphore):
+async def fetch_ids_for_ticker(ticker: str, semaphore: asyncio.Semaphore):
     async with semaphore:
-        year_id2title = defaultdict(dict)
-        total_numIds = 0
         ticker = ticker.lower()
-        ticker_id_folder = os.path.join(id_folder, ticker)
-        if not os.path.exists(ticker_id_folder):
-            os.makedirs(ticker_id_folder)
-            
-        for year in range(year_until, year_since, -1):
-            id2title = defaultdict(lambda: 'title')
-            year_file_path = os.path.join(ticker_id_folder, f"{year-1}.json")
-            if not os.path.exists(year_file_path):
-                continue
-            with open(year_file_path, 'r') as json_file:
-                json_data = json.load(json_file)
-                if 'data' in json_data and json_data['data']:                
-                    for i in range(len(json_data['data'])):
-                        if json_data['data'][i]['type'] == 'transcript':
-                            id = json_data['data'][i]['id']
-                            title = json_data['data'][i]['attributes']['title']
-                            id2title[id] = title
-            year_id2title[year] = id2title
-            year_ids_counts = len(id2title)
-            total_numIds += year_ids_counts
-            # save annual counts
-            if not os.path.exists(save_folder):
-                os.makedirs(save_folder)
-            save_path = os.path.join(save_folder, f'{ticker}_transcript_count.txt')
-            with open(save_path, 'a') as file:
-                file.write(f"{year}: {year_ids_counts}\n")
-                
-            
-        # save counts
-        if not os.path.exists(save_folder):
-            os.makedirs(save_folder)
-        save_path = os.path.join(save_folder, f'{ticker}_transcript_count.txt')
-        with open(save_path, 'a') as file:
-            file.write(f"Total Count: {total_numIds}\n")
-            
+        
+        id2title = defaultdict(lambda: 'title')
+        year_file_path = os.path.join(id_folder, f"{ticker}_transcript_ids.json")
+        with open(year_file_path, 'r') as json_file:
+            json_data = json.load(json_file)
+            if 'data' in json_data and json_data['data']:                
+                for i in range(len(json_data['data'])):
+                    if json_data['data'][i]['type'] == 'transcript':
+                        id = json_data['data'][i]['id']
+                        title = json_data['data'][i]['attributes']['title']
+                        id2title[id] = title
+        id_counts = len(id2title)
         with open('sp500_transcripts_counts.txt', 'a') as file:
-            file.write(f"{ticker}:{total_numIds}\n ")
-
-
+            file.write(f"{ticker}:{id_counts}\n ")
 
 
 async def main():
@@ -87,7 +59,7 @@ async def main():
     for i in range(0, len(tickers), BATCH_SIZE):
         batch = tickers[i:i + BATCH_SIZE]
         print(f"Processing batch {i // BATCH_SIZE + 1}: {batch}")
-        tasks = [fetch_ids_for_ticekr(ticker, semaphore) for ticker in batch]
+        tasks = [fetch_ids_for_ticker(ticker, semaphore) for ticker in batch]
         await asyncio.gather(*tasks)
             
 if __name__ == '__main__':
